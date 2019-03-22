7205cb80273bb2e5c9016ce5268fcb35b1e8a8cf
Elvis Alcántara Pinedo <aelvis@users.noreply.github.com>
2019-03-22 13:05:37 -0500

    update
    update

--- src/app/component/atencion/atencion.component.ts
+++ src/app/component/atencion/atencion.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-atencion',
   templateUrl: './atencion.component.html',
   styleUrls: ['./atencion.component.css']
 })
 export class AtencionComponent implements OnInit {
+  public atencion;
+  constructor(private toastr: ToastrService, private _usuarioService: UsuarioService, private _router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit(){
+		this.obtenerProducto();
+	}
+	showSuccess(titulo,mensaje) {
+    	this.toastr.success(mensaje, titulo);
+  	}
+  	showError(titulo,mensaje) {
+    	this.toastr.error(mensaje, titulo);
+  	}
+	obtenerProducto(){
+		this._usuarioService.obtenerAtencion().subscribe(
+			res => {
+				if(res["mensaje"].terminar){
+				  	localStorage.clear();
+				  	this._router.navigate(['/login']);
+				}else{
+					if(res["mensaje"].atencion){
+						this.atencion = res["mensaje"].atencion;
+					}else{
+						this.atencion = "No hay productos...";
+					}
+				}
+			},
+			error => {
+				this.showSuccess("Alerta","Error de Internet");
+			}
+		);
+	}
 
 }
